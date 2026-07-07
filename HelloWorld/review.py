0f9a8379f5ac5132026abee1e780775affb30c20
Pritam Gurung <psypg6@nottingham.ac.uk>
2026-07-07 17:52:35 +0100

    Completed Basic Project Syntax

---
+++ HelloWorld/review.py
@@ -0,0 +1,7 @@
+my_age = 22 
+half_my_age = my_age / 2 
+greeting = "Hello, how have you been,"
+name = " my name is Pritam Gurung "
+
+greeting_with_name = greeting + name 
+print(greeting_with_name)
---
+++ implement_basic_syntax_project/ascii_project_1.py
@@ -0,0 +1,10 @@
+# Name: Pritam 
+
+
+print("PPPPP")
+print("P    P")
+print("P    P")
+print("PPPPP")
+print("p")
+print("P")
+print("p")
